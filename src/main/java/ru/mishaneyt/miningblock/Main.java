f3e486a61079f60557163b40e94f6f4e708a1994
MishaNeYT <mishapavlov987@gmail.com>
2022-01-21 18:08:14 +0900

    Удалены некоторые разделы и оптимизированы классы

--- src/main/java/ru/mishaneyt/miningblock/Main.java
+++ src/main/java/ru/mishaneyt/miningblock/Main.java
@@ -9,7 +9,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
